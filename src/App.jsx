5a729df0df5bcce711108e32dc5380d4e5c0c1e7
Hasib <hasibulhasan2048@gmail.com>
2025-08-24 00:17:45 +0600

    Nav Bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Nav from './components/Nav'
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
-    <div>
-      <Nav />
-    </div>
+    <main>
+      <NavBar />
+      <HeroSection />
+    </main>
   )
 }
 
