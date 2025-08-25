003730e9459860f6c080a8a202265a0f400c93e0
Hasib <hasibulhasan2048@gmail.com>
2025-08-25 08:47:56 +0600

    hero animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import gsap from "gsap"
+import {ScrollTrigger} from "gsap/all"
+import MessageSecton from './sections/MessageSecton'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <MessageSecton />
     </main>
   )
 }
---
+++ src/sections/MessageSecton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageSecton = () => {
+  return (
+    <section className='message-content'>
+
+    </section>
+  )
+}
+
+export default MessageSecton
